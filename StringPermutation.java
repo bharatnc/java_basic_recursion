e37737877486546ef189634976325453f929bd01
Bharat Nallan <bharatnc@gmail.com>
2017-02-24 08:52:13 -0700

    fixed indentation

--- StringPermutation.java
+++ StringPermutation.java
@@ -7,8 +7,7 @@ public class StringPermutation {
   public static void perm(String prefix, String temp) {
     if (temp.isEmpty()) {
       System.out.println(prefix);
-    } 
-    else {
+    } else {
       for (int i = 0; i < temp.length(); i++) {
         perm(prefix + temp.charAt(i), temp.substring(0,i) + temp.substring(i+1,temp.length()));
       }
